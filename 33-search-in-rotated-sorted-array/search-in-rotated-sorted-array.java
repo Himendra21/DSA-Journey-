7651b3b67ad11a68c718a251e977da2e7f984018
Himendra Gusain <himendragusain@gmail.com>
2026-06-11 17:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (10.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,17 +1,30 @@
 class Solution {
-    // Function to search target in rotated sorted array using brute force
-    public int search(int[] nums, int target) {
+     public int search(int[] nums, int target) {
+         int low = 0;
+         int high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-        // Loop through each element in the array
-        for (int i = 0; i < nums.length; i++) {
-
-            // If current element matches target, return index
-            if (nums[i] == target) {
-                return i;
-            }
+            if (nums[mid] == target)
+            return mid;
+            
+            if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid - 1;
+            } else { 
+                low = mid + 1;
+            } 
         }
 
-        // If not found, return -1
-        return -1;
+            else { if (nums[mid] < target && target <= nums[high]) {
+             low = mid + 1;
+        }
+        else {
+             high = mid - 1;
+        }
     }
 }
+     return -1;
+    }
+  }
